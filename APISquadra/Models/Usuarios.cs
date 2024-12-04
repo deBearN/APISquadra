0ddf7c2f0368580c6b3788824fe3f275fb0525db
Bear <bernardos2002@gmail.com>
2024-12-04 12:22:30 -0300

    Add project files.

---
+++ APISquadra/Models/Usuarios.cs
@@ -0,0 +1,13 @@
+﻿namespace APISquadra.Models
+{
+    public class Usuarios
+    {
+        public Guid userId { get; set; }
+        public string userName { get; set; }
+        public string userEmail { get; set; }
+        public string userPassword { get; set; }
+        public string userPhone { get; set; }
+        public string user
+
+    }
+}
