87b6d93725a71d0200f7a5a1c23f2f2f42e44dde
Bear <bernardos2002@gmail.com>
2024-12-06 10:21:17 -0300

    Preparing Database

---
+++ APISquadra/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using APISquadra.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APISquadra.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions) 
+        {
+
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+    
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuario>()
+                .HasKey(x => x.userId);
+            modelBuilder.Entity<Produto>()
+                .HasKey(x => x.ProdutoID);
+        }
+    }
+}
